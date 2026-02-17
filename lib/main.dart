720d11df9a4f976529849e5c3e2a9ac1f58a3d00
Patrícia Silva <patricia.silv.costa@gmail.com>
2026-02-17 16:22:47 -0300

    termino prévio
    terei que ver os restos das aulas ainda

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,12 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(height: 50),
+                SizedBox(height: 80),
                 CircleAvatar(
                   backgroundImage: ExactAssetImage(
                     'assets/images/fotoperfil.jpg',
                   ),
-                  radius: 50,
+                  radius: 70,
                 ),
 
                 Text(
@@ -36,8 +36,7 @@ class MyApp extends StatelessWidget {
                 ),
 
                 Text(
-                  'Aluna de Sistemas de Informação IFMG - sje'
-                      .toUpperCase(),
+                  'Aluna de Sistemas de Informação IFMG - sje'.toUpperCase(),
                   style: TextStyle(
                     color: Colors.deepOrange.shade100,
                     fontSize: 25,
@@ -46,6 +45,88 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                   textAlign: TextAlign.center,
+                ),
+
+                Center(
+                  child: Container(
+                    width: 300,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      color: Colors.deepOrange.shade100,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 15),
+
+                Center(
+                  child: Container(
+                    width: 370,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFDFFFC),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(Icons.call, size: 40, color: Colors.deepOrange),
+
+                        SizedBox(width: 15),
+
+                        Text(
+                          '(33) 99833-8278',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Source Sans 3',
+                            color: Colors.deepOrange,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 30),
+
+                Center(
+                  child: Container(
+                    width: 370,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFDFFFC),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 10),
+
+                        Icon(
+                          Icons.contact_mail,
+                          size: 40,
+                          color: Colors.deepOrange,
+                        ),
+
+                        SizedBox(width: 15),
+
+                        Text(
+                          'patricia.silv.costa@gmail.com',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Source Sans 3',
+                            color: Colors.deepOrange,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
