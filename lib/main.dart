ffb6617d176edaac740da84fdef995234390ed53
Patrícia Silva <patricia.silv.costa@gmail.com>
2026-02-19 21:27:38 -0300

    fim

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
                 ),
 
                 Text(
-                  'Patricia Silva',
+                  'Patrícia Silva',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 35,
@@ -47,84 +47,48 @@ class MyApp extends StatelessWidget {
                   textAlign: TextAlign.center,
                 ),
 
-                Center(
-                  child: Container(
-                    width: 300,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      color: Colors.deepOrange.shade100,
+                SizedBox(height: 25, width: 300,
+                child: Divider(color: Colors.deepOrange.shade100,),),
+
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.call,
+                      size: 35,
+                      color: Colors.deepOrange,
+                    ),
+                    title: Text(
+                      '(33) 99833-8278',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Source Sans 3',
+                        color: Colors.deepOrange,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
 
-                SizedBox(height: 15),
-
-                Center(
-                  child: Container(
-                    width: 370,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFFDFFFC),
-                      borderRadius: BorderRadius.circular(5),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.contact_mail,
+                      size: 35,
+                      color: Colors.deepOrange,
                     ),
-
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Icon(Icons.call, size: 40, color: Colors.deepOrange),
-
-                        SizedBox(width: 15),
-
-                        Text(
-                          '(33) 99833-8278',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: 'Source Sans 3',
-                            color: Colors.deepOrange,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-
-                SizedBox(height: 30),
-
-                Center(
-                  child: Container(
-                    width: 370,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFFDFFFC),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(width: 10),
-
-                        Icon(
-                          Icons.contact_mail,
-                          size: 40,
-                          color: Colors.deepOrange,
-                        ),
-
-                        SizedBox(width: 15),
-
-                        Text(
-                          'patricia.silv.costa@gmail.com',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: 'Source Sans 3',
-                            color: Colors.deepOrange,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                      ],
+                    title: Text(
+                      'patricia.silv.costa@gmail.com',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Source Sans 3',
+                        color: Colors.deepOrange,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
